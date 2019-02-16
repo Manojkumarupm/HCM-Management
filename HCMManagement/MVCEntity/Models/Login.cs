7026f2e79ae3cde9fd770e6f6514227f41ff2860
Manoj <manojkumarupm60@gmail.com>
2019-02-16 02:19:01 -0500

    HCM
    HCM FSE Program Changes

---
+++ HCMManagement/MVCEntity/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCEntity.Models
+{
+    public partial class Login
+    {
+
+        [Required(ErrorMessage = "User Name is Required")]
+
+        public string UserName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Password is Required")]
+
+        public string Password { get; set; }
+
+    }
+}
